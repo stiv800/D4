4018a560c560294ee0afecbaa230f0b2b90783af
Stiv800 <stpab@mail.ru>
2022-02-12 19:03:56 +0300

    Новые возможности форм

--- news/forms.py
+++ news/forms.py
@@ -6,4 +6,5 @@ class PostsForm(ModelForm):
     class Meta:
         model = Post
         fields = ['author', 'title', 'text', 'postCategorys', 'categoryType']
+        labels = {'author':'Автор', 'title':'Заголовок', 'text':'Текс', 'postCategorys':'Категория', 'categoryType':'Тип'}
         
